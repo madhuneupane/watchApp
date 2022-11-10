a3f9d6f7d6b873c506dbc66a4ba447afb4267ff3
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 12:15:40 -0800

    Adjustments to Dropdown component
    Adding styles (these will be moved later to a specific stylesheet).

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="nav-link" @click.prevent="isOpen = !isOpen">
+    <div class="nav-link" id="drop" @click.prevent="isOpen = !isOpen">
         <a href='#'>{{ title }}</a>
         <img class="icon" src="../assets/icons/dropdown.svg" />
         <div class="drop-down" v-if="isOpen">
             <div class="sub-menu">
-                <router-link to="/OngoingChallenges">Ongoing Challenges</router-link>
+                <router-link to="/challenge-main">Challenges</router-link>
+            </div>
+            <div class="sub-menu">
+                <router-link to="/ongoing-challenges">Ongoing Challenges</router-link>
             </div>
             <div class="sub-menu">
                 <router-link to="/challenges-main/past-challenges">Past Challenges</router-link>
             </div>
             <div class="sub-menu">
-                <router-link to="/CreateChallenges">Create Challenges</router-link>
+                <router-link to="/create-challenge">Create Challenges</router-link>
             </div>
         </div>
     </div>
@@ -27,3 +30,23 @@ export default {
     },
 }
 </script>
+
+<style>
+.drop-down {
+    position: absolute;
+    background-color: #7F11C3;
+    border: 1px solid white;
+    z-index: 2;
+    transform: translateX(20px);
+    border-radius: 5px;
+}
+
+.sub-menu {
+    padding: .3rem;
+    transition: all 0.2s ease-in-out;
+}
+
+.sub-menu:hover {
+    background-color: #5F01A3;
+}
+</style>
