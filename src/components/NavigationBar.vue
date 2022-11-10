01c11942495d4f4f0a9ec6719f64ffb7d37ab03c
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 12:18:24 -0800

    Update NavigationBar
    Updating the function and including profile picture placeholder and styles (these will be moved later).

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="navbar navbar-gradient">
-        <img src="../assets/icons/whitelogo.svg" alt="watchapp">
+        <img id="logo" src="../assets/icons/whitelogo.svg" alt="watchapp">
         <div class="navbar-items">
-            <router-link class="nav-link" to="/TestTest">Home</router-link>
-            <DropdownMenu title="Challenges" />
+            <router-link class="nav-link" id="home" to="/">Home</router-link>
+            <DropdownMenu v-if="isSignedIn" title="Challenges" />
+            <!-- v-if="user" -->
+            <router-link v-if="!isSignedIn" class="nav-link" id="login" to="/login-page">Login</router-link>
+            <img v-if="isSignedIn" id="userIcon" src="../assets/icons/1538515088893.jpeg" alt="userPic"
+                @click.prevent="profile">
+            <!-- <router-link v-if="isSignedIn" @click.prevent="signOut" class="nav-link" id="signOut">Sign
+                Out
+            </router-link> -->
+            <!-- Navbar disappears -->
         </div>
-        <router-link to="/LoginPage">Login</router-link>
     </div>
 </template>
 
@@ -17,11 +24,48 @@ export default {
     components: {
         DropdownMenu,
     },
+    data() {
+        return {
+            user: "",
+            isSignedIn: true
+        }
+    },
+    methods: {
+        profile() {
+            this.$router.push("/profile-page");
+        },
+        //     signOut() {
+        //         this.isSignedIn = false;
+        //         this.$router.push("/");
+        //     }
+    }
 }
 </script>
 
 <style>
-.navbar-gradient {
-    background: linear-gradient(91.25deg, blue 0%, purple 51.47%, orange 100%);
+#userIcon {
+    width: 70px;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+.navbar-items {
+    display: inline-flex;
+    align-items: center;
+    padding: 5px;
+}
+
+.nav-link {
+    margin-left: 40px;
+    margin-right: 40px;
+}
+
+.icon {
+    width: 20px;
+    padding-left: 5px;
+}
+
+#logo {
+    padding-left: 20px;
 }
 </style>
