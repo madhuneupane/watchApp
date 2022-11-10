904bd905b8237b5b3efa411e2bb45e2690d2fba1
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 12:43:07 -0800

    Create Forgot Email page

---
+++ src/page-templates/ForgotEmail.vue
@@ -0,0 +1,22 @@
+<template>
+    <NavigationBar />
+    <h1>Retrieve your email</h1>
+    <p>Please contact the administrator to retrieve your email or reach us at <a style="textDecoration: underline"
+            id="fakeEmail" href="mailto:help@watchapp.com">help@watchapp.com</a></p>
+    <FooterBar />
+</template>
+
+
+<script>
+import NavigationBar from "../components/NavigationBar.vue";
+import FooterBar from "../components/FooterBar.vue";
+
+export default {
+    name: "ForgotEmail",
+    components: {
+        NavigationBar,
+        FooterBar
+    }
+}
+
+</script>
