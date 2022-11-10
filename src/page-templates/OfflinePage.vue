49dc03a0276af9efb1ba0acf7316a78611f7c1f5
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 12:40:59 -0800

    Create Offline Page
    Missing connection with the project.

---
+++ src/page-templates/OfflinePage.vue
@@ -0,0 +1,19 @@
+
+<template>
+<div class="offline">
+<h1>Error</h1>
+<h1>Something went wrong. Please try again later</h1>
+<button type="submit" class="primaryBtn" @click.prevent="">
+Reload Page
+</button>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "OfflinePage"
+}
+
+
+</script>
