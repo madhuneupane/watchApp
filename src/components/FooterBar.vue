3406d96686350642e3b825edf7bd8acd292440e3
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 12:41:28 -0800

    Add router-link to About Us page

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -3,7 +3,7 @@
         <img src="../assets/icons/whitelogo.svg" alt="watchapp">
         <h4>The Seven Knights</h4>
         <a href="">Contact Us</a>
-        <a href="">About Us</a>
+        <router-link to="/about-us">About Us</router-link>
         <span>100 W 49th Ave, Vancouver, BC V5Y 2Z6</span>
         <span>The Seven Knights. All rights reserved</span>
     </footer>
