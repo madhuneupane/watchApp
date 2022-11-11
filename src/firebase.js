40131a45ddc33d2249774f3e4a7c01eab32bda93
Marina Faria <marina.acfaria@gmail.com>
2022-11-11 12:36:47 -0800

    Updates on firebase.js
    Important updates with auth.

--- src/firebase.js
+++ src/firebase.js
@@ -1,35 +1,10 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDfg85yR0oSB-g6C-t_U4VWSbTafB0X3Lo",
-//   authDomain: "test-6a30f.firebaseapp.com",
-//   projectId: "test-6a30f",
-//   storageBucket: "test-6a30f.appspot.com",
-//   messagingSenderId: "54325834329",
-//   appId: "1:54325834329:web:af78d33075ae3861297748",
-//   measurementId: "G-82D9NTK341",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// //const analytics = getAnalytics(app);
-// const db = getFirestore(app);
-
-// export { db };
+// IMPORTING FROM FIREBASE
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-// Marina
-import "firebase/auth";
+import { getAuth } from "firebase/auth";
 import "firebase/storage";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyASdmqlaScVgkSxCrvYng7_SzRnE2VQRgU",
   authDomain: "app1-504b3.firebaseapp.com",
@@ -44,12 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
-const db = getFirestore(app);
-
-// Marina
-// const auth = getAuth(app);
-
-export { db };
-
-// Marina
-// export { auth };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
