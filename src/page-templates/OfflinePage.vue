827411c4097af17648d91c4696de64ccd0ccdd97
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 23:04:47 -0800

    Inclusion of Offline Page
    Still missing connection with the page.

--- src/page-templates/OfflinePage.vue
+++ src/page-templates/OfflinePage.vue
@@ -1,19 +1,30 @@
-
 <template>
-<div class="offline">
-<h1>Error</h1>
-<h1>Something went wrong. Please try again later</h1>
-<button type="submit" class="primaryBtn" @click.prevent="">
-Reload Page
-</button>
-</div>
+  <NavigationBar />
+  <div class="offline">
+    <img src="../assets/icons/camera-roll.svg" alt="cameraRoll">
+    <h1>Error</h1>
+    <h2>Something went wrong. Please try again later</h2>
+    <button type="submit" class="primaryBtn" @click.prevent="reload">
+      Reload Page
+    </button>
+  </div>
+  <FooterBar />
 </template>
 
 <script>
+import NavigationBar from '../components/NavigationBar.vue';
+import FooterBar from "../components/FooterBar.vue";
 
 export default {
-  name: "OfflinePage"
+  name: "OfflinePage",
+  components: {
+    NavigationBar,
+    FooterBar
+  },
+  methods: {
+    reload() {
+      this.$router.push("/");
+    }
+  }
 }
-
-
 </script>
