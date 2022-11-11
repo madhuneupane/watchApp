9cf2569a46258d9a384035c288f27ebe0b14a828
Marina Faria <marina.acfaria@gmail.com>
2022-11-11 12:22:16 -0800

    Sign out button method update
    Please notice that the v-if still needs some work.

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,21 +2,25 @@
     <div class="navbar navbar-gradient">
         <img id="logo" src="../assets/icons/whitelogo.svg" alt="watchapp">
         <div class="navbar-items">
-            <router-link class="nav-link" id="home" to="/">Home</router-link>
-            <DropdownMenu v-if="isSignedIn" title="Challenges" />
+            <div>
+                <div class="nav-link" id="home" @click.prevent="goToHome">Home</div>
+                <DropdownMenu v-if="isSignedIn" title="Challenges" />
+            </div>
             <!-- v-if="user" -->
-            <router-link v-if="!isSignedIn" class="nav-link" id="login" to="/login-page">Login</router-link>
+            <div v-if="!isSignedIn" class="nav-link" id="login" @click.prevent="goToLogin">Login</div>
             <img v-if="isSignedIn" id="userIcon" src="../assets/icons/1538515088893.jpeg" alt="userPic"
                 @click.prevent="profile">
-            <!-- <router-link v-if="isSignedIn" @click.prevent="signOut" class="nav-link" id="signOut">Sign
+            <button v-if="isSignedIn" @click.prevent="signingOut" class="nav-link tertiaryBtn" id="signOut">Sign
                 Out
-            </router-link> -->
-            <!-- Navbar disappears -->
+            </button>
         </div>
     </div>
 </template>
 
 <script>
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
+
 import DropdownMenu from "./DropdownMenu.vue";
 
 export default {
@@ -26,7 +30,6 @@ export default {
     },
     data() {
         return {
-            user: "",
             isSignedIn: true
         }
     },
@@ -34,15 +37,40 @@ export default {
         profile() {
             this.$router.push("/profile-page");
         },
-        //     signOut() {
-        //         this.isSignedIn = false;
-        //         this.$router.push("/");
-        //     }
+        goToHome() {
+            this.$router.push("/");
+        },
+        goToLogin() {
+            this.$router.push("/login-page");
+        },
+        async signingOut() {
+            signOut(auth)
+                .then(() => {
+                    console.log("Sign out successful");
+                    this.isSignedIn = false;
+                    this.$router.push("/");
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        }
     }
 }
 </script>
 
 <style>
+.navbar {
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+}
+
+@media only screen and (min-width: 1024px) {
+    .navbar {
+        flex-direction: row;
+    }
+}
+
 #userIcon {
     width: 70px;
     border-radius: 50%;
@@ -68,4 +96,10 @@ export default {
 #logo {
     padding-left: 20px;
 }
+
+#home,
+#login {
+    cursor: pointer;
+}
 </style>
+
