03cd8a0d08ba92f11cd8a357f1fec4aeda9419a2
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 23:00:38 -0800

    Update on component Recommendation Movies

--- src/components/RecommendationMovies.vue
+++ src/components/RecommendationMovies.vue
@@ -1,25 +1,15 @@
 <template>
     <section id="recommendationSec">
-    <div class="carouselContainer" v-if="carouselKey">
-        <div class="headContainer">
-            <h2>You will like this!</h2>
-            <a class="seeMoreBtn" @click.prevent="gotoRecommendationsPage">See All</a>
-        </div>
-        <vueper-slides
-        :arrows="false"
-        :infinite="false"
-        :bullets="false"
-        :visible-slides="5"
-        :slide-multiple="2"
-        :gap="2"
-        :slide-ratio="1 / 3.5"
-        :dragging-distance="200"
-        :breakpoints="breakpoints">
-            <vueper-slide v-for="movie in recommendationMovieList" 
-            :key="movie" 
-            :title="movie.original_title"
-            :image="'https://image.tmdb.org/t/p/w500'+movie.poster_path" />
-        </vueper-slides>
+        <div class="carouselContainer" v-if="carouselKey">
+            <div class="headContainer">
+                <h2>You will like this!</h2>
+                <a class="seeMoreBtn" @click.prevent="gotoRecommendationsPage">See All</a>
+            </div>
+            <vueper-slides :arrows="false" :infinite="false" :bullets="false" :visible-slides="5" :slide-multiple="2"
+                :gap="2" :slide-ratio="1 / 3.5" :dragging-distance="200" :breakpoints="breakpoints">
+                <vueper-slide v-for="movie in recommendationMovieList" :key="movie"
+                    :image="'https://image.tmdb.org/t/p/w500' + movie.poster_path" />
+            </vueper-slides>
         </div>
     </section>
 </template>
@@ -38,8 +28,8 @@ export default {
     data() {
         return {
             recommendationMovieList: [],
-            breakpoints: { 
-                1023: { visibleSlides: 4, slideMultiple: 2, slideRatio: 1 / 3}
+            breakpoints: {
+                1024: { visibleSlides: 1, slideMultiple: 2, slideRatio: 1 / 3 }
             },
             carouselKey: false,
         }
@@ -47,13 +37,13 @@ export default {
 
     created() {
         this.recommendationMovieList = axios.get("https://api.themoviedb.org/3/movie/popular?api_key=8ec942643846f64d66eed102868455f3&language=en-US&page=1&region=US")
-        .then((info) => {
-            this.recommendationMovieList = info.data.results;
-        })
-        .then(() => {
-            this.recommendationMovieList = JSON.parse(JSON.stringify(this.recommendationMovieList))
-            this.forceRerender();
-        });
+            .then((info) => {
+                this.recommendationMovieList = info.data.results;
+            })
+            .then(() => {
+                this.recommendationMovieList = JSON.parse(JSON.stringify(this.recommendationMovieList))
+                this.forceRerender();
+            });
     },
 
     methods: {
