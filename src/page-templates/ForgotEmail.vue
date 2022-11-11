65ece51f9d1da96caec57ea16948f97a756a9298
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 23:05:10 -0800

    Update ForgotEmail.vue

--- src/page-templates/ForgotEmail.vue
+++ src/page-templates/ForgotEmail.vue
@@ -3,6 +3,8 @@
     <h1>Retrieve your email</h1>
     <p>Please contact the administrator to retrieve your email or reach us at <a style="textDecoration: underline"
             id="fakeEmail" href="mailto:help@watchapp.com">help@watchapp.com</a></p>
+
+    <router-link id="backToLogin" to="/login-page">Back to login page</router-link>
     <FooterBar />
 </template>
 
@@ -18,5 +20,4 @@ export default {
         FooterBar
     }
 }
-
 </script>
