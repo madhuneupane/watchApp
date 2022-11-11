198ae300fabb8ec27b2dc4240629c02b6c022bc6
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 23:01:32 -0800

    Update on 404 Page
    Adjustment of tags and inclusion of functionality.

--- src/page-templates/404Page.vue
+++ src/page-templates/404Page.vue
@@ -1,19 +1,37 @@
 <template>
-<div class="404">
-<h1>Oops!</h1>
-<h1>We can't seem to find the page you are looking for.</h1>
-<p>Don't Worry! You can try these useful links instead:</p>
-<button type="submit" class="primaryBtn" @click.prevent="Home">
-Home
-</button>
-<button type="submit" class="secondaryBtn" @click.prevent="Goback">
-Go Back
-</button>
-</div>
+  <NavigationBar />
+  <div class="404">
+    <h1>Oops!</h1>
+    <h2>We can't seem to find the page you are looking for.</h2>
+    <p>Don't worry! You can try these useful links instead:</p>
+    <button type="submit" class="secondaryBtn" @click.prevent="home">
+      Home
+    </button>
+    <button type="submit" class="primaryBtn" @click.prevent="goBack">
+      Go Back
+    </button>
+  </div>
+  <FooterBar />
 </template>
 
 <script>
+import NavigationBar from "../components/NavigationBar.vue";
+import FooterBar from "../components/FooterBar.vue";
+
 export default {
-  name: "ErrorPage"
+  name: "ErrorPage",
+  components: {
+    NavigationBar,
+    FooterBar
+  },
+  methods: {
+    home() {
+      this.$router.push("/");
+    },
+    goBack() {
+      this.$router.push("/");
+      // Necessary to check how to go back to the page that generated the error.
+    }
+  }
 }
 </script>
