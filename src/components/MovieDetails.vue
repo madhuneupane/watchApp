9ab8e26d151b67e2d61b2309cd0129346776fe7e
Marina Faria <marina.acfaria@gmail.com>
2022-11-10 22:59:08 -0800

    Movie Details pop-up
    Pop-up to be implemented.

---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,49 @@
+<template>
+  <img class="movieClose icon" src="../assets/icons/close.svg" alt="broke" width="20rem" />
+  <div class="movieWrapper">
+    <div class="movieDetailImage">
+      <img src="https://image.tmdb.org/t/p/w500/spCAxD99U1A6jsiePFoqdEcY0dG.jpg" alt="posterImage" width="350" />
+    </div>
+    <div class="movieDetailInfo">
+      <h2>Thor: Love and Thunder</h2>
+      <h3>Release Date: 07/03/2019</h3>
+      <h3>TMDB Rating: 6.8/10</h3>
+    </div>
+  </div>
+  <div class="movieOverview">
+    <h2>Overview</h2>
+    <p style="line-height: 1.5">
+      Lorem ipsum dolr sit amet consectetur adipisicing elit. Odit ad illum
+      similique quibusdam doloremque molestias at enim rem hic cum corporis quae
+      iusto, error deleniti, veritatis consequuntur nesciunt accusantium
+      tenetur, necessitatibus vel cupiditate culpa eius explicabo. Quod quaerat
+      corporis quos aut veniam corrupti, vitae eos qui accusamus omnis
+      voluptates commodi ab inventore dolorum fugiat similique! Cupiditate
+      ratione recusandae, optio explicabo sit eligendi eaque reprehenderit dicta
+      est officiis labore? Cumque, qui nihil! Minima perspiciatis corrupti
+      voluptates, ex unde velit similique labore numquam nihil animi, sunt
+      aliquam consectetur eaque pariatur doloremque quo, ea nemo architecto
+      doloribus voluptatum. Nemo veritatis minima soluta? Architecto quas
+      accusantium quibusdam aut provident voluptatibus nisi officia consectetur
+      quos, eligendi ut nesciunt libero quo ipsa? Labore odit vero libero
+      corporis itaque ex nemo recusandae ratione cumque quibusdam. Quae
+      praesentium consectetur eos. Vitae aliquam ipsum similique repudiandae
+      rerum expedita commodi molestias dolorum sequi voluptatum, incidunt
+      perferendis totam, mollitia optio. Voluptas asperiores, velit iure ratione
+      cupiditate cum vero iusto quia inventore voluptate quam nulla, quae ut.
+      Repellendus maiores voluptates assumenda expedita explicabo recusandae
+      architecto eaque modi, quae nostrum officia aspernatur mollitia esse
+      itaque sequi aliquid earum illum dolore quaerat vel temporibus enim
+      perspiciatis minus id? Mollitia dicta recusandae corrupti iste veniam.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieDetails",
+};
+</script>
+<style>
+
+</style>
