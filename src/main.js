0eb9f780f1fa8dd66886f9bf1aa60b70f3ce115a
Marina Faria <marina.acfaria@gmail.com>
2022-11-11 23:22:22 -0800

    Import of mitt
    Import of mitt to link other pages.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.scss'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.scss';
+import App from './App.vue';
 import router from "./routes";
-createApp(App).use(router).mount("#app");
+import './registerServiceWorker';
+import mitt from 'mitt';
+
+const emitter = mitt();
+
+const app = createApp(App);
+
+app.config.globalProperties.emitter = emitter;
+app.use(router).mount("#app");
