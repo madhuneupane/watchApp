54500fab37e34c9c9bfa698cbe805070d110ae2d
Marina Faria <marina.acfaria@gmail.com>
2022-11-11 19:49:06 -0800

    Create TestPage for testing new components
    Page is being used to test the Auth from Google and Facebook

---
+++ src/TEST/TestPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Login Auth</h1>
+    <LoginAuth />
+</template>
+
+<script>
+import LoginAuth from "../components/LoginAuth.vue";
+
+export default {
+    name: "TestPage",
+    components: {
+        LoginAuth,
+    }
+}
+</script>
