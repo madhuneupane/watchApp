f4eb8eda79283ac18ee5df597366a0232aa13590
edenLangara <114768729+edenLangara@users.noreply.github.com>
2022-11-12 04:57:26 -0800

    Challanges Carousel added on main page

--- src/page-templates/MainPage.vue
+++ src/page-templates/MainPage.vue
@@ -1,5 +1,6 @@
 <template>
   <NavigationBar />
+  <ChallangesCarousel />
   <UpcomingMovies />
   <RecommendationMovies />
   <h2>Interested in Challenges?</h2>
@@ -8,6 +9,7 @@
 </template>
   
 <script>
+import ChallangesCarousel from "../components/ChallangesCarousel";
 import UpcomingMovies from "../components/UpcomingMovies";
 import RecommendationMovies from "../components/RecommendationMovies.vue";
 import NavigationBar from "../components/NavigationBar.vue";
@@ -17,6 +19,7 @@ export default {
   name: "MainPage",
 
   components: {
+    ChallangesCarousel,
     UpcomingMovies,
     RecommendationMovies,
     NavigationBar,
