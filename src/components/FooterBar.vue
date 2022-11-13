6c85b1d6fc384d9c4d9e68faf25d393eb39adbca
Marina Faria <marina.acfaria@gmail.com>
2022-11-13 01:14:19 -0800

    Inclusion of Contact Us in FooterBar

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -2,7 +2,7 @@
     <footer id="footer">
         <img src="../assets/icons/whitelogo.svg" alt="watchapp">
         <h4>The Seven Knights</h4>
-        <a href="">Contact Us</a>
+        <router-link to="/contact-us">Contact Us</router-link>
         <router-link to="/about-us">About Us</router-link>
         <span>100 W 49th Ave, Vancouver, BC V5Y 2Z6</span>
         <span>The Seven Knights. All rights reserved</span>
