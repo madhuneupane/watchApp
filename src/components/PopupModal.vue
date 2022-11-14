6753c1954c57cdf40037b847c76def471f5959ce
Marina Faria <marina.acfaria@gmail.com>
2022-11-13 23:17:29 -0800

    Create component pop-up

---
+++ src/components/PopupModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <transition name="popupAnimation">
+        <div v-show="popupActive" class="popup">
+            <transition name="popupAnimationInner">
+                <div v-show="popupActive" class="popupInner">
+                    <img @click="close" src="../assets/icons/close.svg" alt="close" id="closePopup">
+                    <!-- Modal content -->
+                    <slot />
+                    <button @click="close" type="button" class="secondaryBtn">Close</button>
+                </div>
+            </transition>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    props: ["popupActive"],
+    setup(props, { emit }) {
+        const close = () => {
+            emit("close");
+        }
+        return { close };
+    },
+}
+
+</script>
+
