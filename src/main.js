6ab7fbb3acf807e72f6d82f714040aeff10d8e24
Marina Faria <marina.acfaria@gmail.com>
2022-11-15 15:57:22 -0800

    Update on main.js
    As we aren't using the emitter, this was commented.

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import './style.scss';
 import App from './App.vue';
 import router from "./routes";
 import './registerServiceWorker';
-import mitt from 'mitt';
+// import mitt from 'mitt';
 
-const emitter = mitt();
+// const emitter = mitt();
 
 const app = createApp(App);
 
-app.config.globalProperties.emitter = emitter;
+// app.config.globalProperties.emitter = emitter;
 app.use(router).mount("#app");
