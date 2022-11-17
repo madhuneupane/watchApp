2deb1ef74f1a4e79a313bd03783d0ea29e64e83c
Marina Faria <marina.acfaria@gmail.com>
2022-11-16 23:24:01 -0800

    Update on main.js for inclusion of Vue Star Rating

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import './style.scss';
 import App from './App.vue';
 import router from "./routes";
 import './registerServiceWorker';
-// import mitt from 'mitt';
+import vue3StarRatings from "vue3-star-ratings";
 
+// import mitt from 'mitt';
 // const emitter = mitt();
 
 const app = createApp(App);
 
 // app.config.globalProperties.emitter = emitter;
 app.use(router).mount("#app");
+app.component("vue3-star-ratings", vue3StarRatings);
