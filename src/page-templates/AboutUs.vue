0fbe814daaee96c50ec3c5d0ad83ffa1d2c4c54e
Marina Faria <marina.acfaria@gmail.com>
2022-11-18 14:22:15 -0800

    Add About Us text

--- src/page-templates/AboutUs.vue
+++ src/page-templates/AboutUs.vue
@@ -2,8 +2,14 @@
   <NavigationBar />
   <div id="aboutUs">
     <h1>Meet The Seven Knights!</h1>
-    <p>Loream ipsum dolor sit amet, consectetur adipiscing elt, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua</p>
+    <p>As nowadays we live in a flood of information, sometimes can be difficult to find the information we really want.
+      The same is applied to movies, when we usually take a long time to find the program we actually want to see or we
+      become lost considering the amount of movies that are released every single month, and how to get all the
+      information provided.</p>
+    <p>We, Seven Knights, exist to provide users with essential movie information and also the pleasure of watching
+      movies. WatchApp, created by Seven Knights, allows users to quickly access the new movie information, recommend
+      movies to suit their tastes, and create challenges to experience a variety of movies, helping users meet the wider
+      movie world.</p>
   </div>
 
   <div class="column">
@@ -11,7 +17,7 @@
       <img src="" alt="Marina">
       <div class="container">
         <h2>Marina</h2>
-        <p class="title">Project Manager</p>
+        <p class="title">Project Manager & Developer</p>
       </div>
     </div>
   </div>
