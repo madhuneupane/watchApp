be7a38d4c36b970ad27bdae198526acdde66d319
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2022-11-18 15:33:55 -0800

    bug fix on the popup

--- src/page-templates/ChallengeMain.vue
+++ src/page-templates/ChallengeMain.vue
@@ -1,29 +1,29 @@
 <template>
-    <NavigationBar />
-    <div class="headContainer">
-        <h1>Challenges</h1>
-    </div>
-    <OngoingCarousel />
-    <AdminCarousel />
-    <PastCarousel />
-    <FooterBar />
+  <NavigationBar />
+  <div class="headContainer">
+    <h1>Challenges</h1>
+  </div>
+  <OngoingCarousel />
+  <AdminCarousel />
+  <PastCarousel />
+  <FooterBar />
 </template>
 
 <script>
-import NavigationBar from '../components/NavigationBar.vue';
+import NavigationBar from "../components/NavigationBar.vue";
 import FooterBar from "../components/FooterBar.vue";
-import OngoingCarousel from '../components/OngoingCarousel.vue';
-import AdminCarousel from '../components/AdminCarousel.vue';
-import PastCarousel from '../components/PastCarousel.vue';
+import OngoingCarousel from "../components/OngoingCarousel.vue";
+import AdminCarousel from "../components/AdminCarousel.vue";
+import PastCarousel from "../components/PastCarousel.vue";
 
 export default {
-    name: "ChallengeMain",
-    components: {
-        NavigationBar,
-        FooterBar,
-        OngoingCarousel,
-        AdminCarousel,
-        PastCarousel
-    },
-}
+  name: "ChallengeMain",
+  components: {
+    NavigationBar,
+    FooterBar,
+    OngoingCarousel,
+    AdminCarousel,
+    PastCarousel,
+  },
+};
 </script>
