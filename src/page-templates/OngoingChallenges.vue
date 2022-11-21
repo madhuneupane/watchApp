0b49e642e904efa80f28064869a899d6da08e615
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2022-11-21 15:56:32 -0800

    Genre Id to Name conversion

--- src/page-templates/OngoingChallenges.vue
+++ src/page-templates/OngoingChallenges.vue
@@ -128,6 +128,85 @@ export default {
       description: "",
       windowSize: 0,
       totalWatched: 0,
+      genreArray: [
+        {
+          id: 28,
+          name: "Action",
+        },
+        {
+          id: 12,
+          name: "Adventure",
+        },
+        {
+          id: 16,
+          name: "Animation",
+        },
+        {
+          id: 35,
+          name: "Comedy",
+        },
+        {
+          id: 80,
+          name: "Crime",
+        },
+        {
+          id: 99,
+          name: "Documentary",
+        },
+        {
+          id: 18,
+          name: "Drama",
+        },
+        {
+          id: 10751,
+          name: "Family",
+        },
+        {
+          id: 14,
+          name: "Fantasy",
+        },
+        {
+          id: 36,
+          name: "History",
+        },
+        {
+          id: 27,
+          name: "Horror",
+        },
+        {
+          id: 10402,
+          name: "Music",
+        },
+        {
+          id: 9648,
+          name: "Mystery",
+        },
+        {
+          id: 10749,
+          name: "Romance",
+        },
+        {
+          id: 878,
+          name: "Science Fiction",
+        },
+        {
+          id: 10770,
+          name: "TV Movie",
+        },
+        {
+          id: 53,
+          name: "Thriller",
+        },
+        {
+          id: 10752,
+          name: "War",
+        },
+        {
+          id: 37,
+          name: "Western",
+        },
+      ],
+
       // uid: ""
     };
   },
@@ -169,7 +248,19 @@ export default {
       // console.log(movieData);
       sessionStorage.setItem("movieName", movieArray.title);
       sessionStorage.setItem("movieRelease", movieArray.release_date);
-      sessionStorage.setItem("movieID", movieArray.genre_ids);
+      let genreName = "";
+      for (let i = 0; i < this.genreArray.length; i++) {
+        for (let j = 0; j < movieArray.genre_ids.length; j++) {
+          if (
+            Number(this.genreArray[i].id) == Number(movieArray.genre_ids[j])
+          ) {
+            genreName += this.genreArray[i].name + ", ";
+          }
+        }
+      }
+      genreName = genreName.slice(0, genreName.length - 2);
+
+      sessionStorage.setItem("movieID", genreName);
       sessionStorage.setItem("movieRating", movieArray.vote_average);
       sessionStorage.setItem("movieOverview", movieArray.overview);
       sessionStorage.setItem("moviePoster", movieArray.poster_path);
--- src/page-templates/UpcomingMoviesPage.vue
+++ src/page-templates/UpcomingMoviesPage.vue
@@ -1,19 +1,38 @@
 <template>
-    <NavigationBar />
-    <section id="upcomingPageSec">
-        <h2>Upcoming Movies</h2>
-        <PopupModal :popupActive=popupActive :popupTitle=popupTitle :popupPoster=popupPoster
-            :popupGenreIDs=popupGenreIDs :popupReleaseDate=popupReleaseDate :popupAvarage=popupAvarage
-            :popupOverview=popupOverview v-on:closeClicked="closePopup" />
-        <div class="movieList">
-            <div class="movieItem" :key="movie" v-for="movie in upcomingMovieList">
-                <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
-                    alt="{{ movie.original_title }} + ' Movie Poster'"
-                    @click="togglePopup(movie.poster_path, movie.original_title, movie.genre_ids, movie.release_date, movie.vote_average, movie.overview)">
-            </div>
-        </div>
-    </section>
-    <!-- <PopupModal @close="togglePopup" :popupActive="popupActive" :key="movie" v-for="movie in upcomingMovieList">
+  <NavigationBar />
+  <section id="upcomingPageSec">
+    <h2>Upcoming Movies</h2>
+    <PopupModal
+      :popupActive="popupActive"
+      :popupTitle="popupTitle"
+      :popupPoster="popupPoster"
+      :popupGenreIDs="popupGenreIDs"
+      :popupReleaseDate="popupReleaseDate"
+      :popupAvarage="popupAvarage"
+      :popupOverview="popupOverview"
+      v-on:closeClicked="closePopup"
+    />
+    <div class="movieList">
+      <div class="movieItem" :key="movie" v-for="movie in upcomingMovieList">
+        <img
+          :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
+          alt="{{ movie.original_title }} + ' Movie Poster'"
+          @click="
+            togglePopup(
+              movie.poster_path,
+              movie.original_title,
+              movie.genre_ids,
+              movie.release_date,
+              movie.vote_average,
+              movie.overview,
+              movie
+            )
+          "
+        />
+      </div>
+    </div>
+  </section>
+  <!-- <PopupModal @close="togglePopup" :popupActive="popupActive" :key="movie" v-for="movie in upcomingMovieList">
         <div class="popupContent">
             <img class="movieImg" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
                 alt="{{ movie.original_title }} + ' Movie Poster'">
@@ -26,7 +45,7 @@
             <p class="movieOverview">{{ movie.overview }}</p>
         </div>
     </PopupModal> -->
-    <FooterBar />
+  <FooterBar />
 </template>
 
 <script>
@@ -38,62 +57,164 @@ import PopupModal from "../components/PopupModal.vue";
 // import { ref } from 'vue';
 
 export default {
-    name: 'UpcomingMoviesPage',
-    components: {
-        NavigationBar,
-        FooterBar,
-        PopupModal
-    },
-    data() {
-        return {
-            upcomingMovieList: [],
-            imgPreURL: 'https://image.tmdb.org/t/p/w500',
-            //For popups
-            popupTitle: "",
-            popupPoster: "",
-            popupGenreIDs: "",
-            popupReleaseDate: "",
-            popupAvarage: "",
-            popupOverview: "",
-            popupActive: false,
-        }
-    },
-
-    created() {
-        this.upcomingMovieList = axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=8ec942643846f64d66eed102868455f3&language=en-US&region=US&page=1")
-            .then((info) => {
-                this.upcomingMovieList = info.data.results;
-            })
-            .then(() => {
-                this.upcomingMovieList = JSON.parse(JSON.stringify(this.upcomingMovieList))
-                this.forceRerender();
-            });
-    },
-
-    methods: {
-        forceRerender() {
-            this.carouselKey += 1;
+  name: "UpcomingMoviesPage",
+  components: {
+    NavigationBar,
+    FooterBar,
+    PopupModal,
+  },
+  data() {
+    return {
+      upcomingMovieList: [],
+      imgPreURL: "https://image.tmdb.org/t/p/w500",
+      //For popups
+      popupTitle: "",
+      popupPoster: "",
+      popupGenreIDs: "",
+      popupReleaseDate: "",
+      popupAvarage: "",
+      popupOverview: "",
+      popupActive: false,
+      genreArray: [
+        {
+          id: 28,
+          name: "Action",
         },
-        togglePopup(posterPath, originalTitle, genreids, releaseDate, voteAverage, overview) {
-            this.popupPoster = posterPath;
-            this.popupTitle = originalTitle;
-            this.popupGenreIDs = genreids;
-            this.popupReleaseDate = releaseDate;
-            this.popupAvarage = voteAverage;
-            this.popupOverview = overview;
-            this.popupActive = true;
+        {
+          id: 12,
+          name: "Adventure",
         },
-        closePopup() {
-            this.popupActive = false;
-        }
+        {
+          id: 16,
+          name: "Animation",
+        },
+        {
+          id: 35,
+          name: "Comedy",
+        },
+        {
+          id: 80,
+          name: "Crime",
+        },
+        {
+          id: 99,
+          name: "Documentary",
+        },
+        {
+          id: 18,
+          name: "Drama",
+        },
+        {
+          id: 10751,
+          name: "Family",
+        },
+        {
+          id: 14,
+          name: "Fantasy",
+        },
+        {
+          id: 36,
+          name: "History",
+        },
+        {
+          id: 27,
+          name: "Horror",
+        },
+        {
+          id: 10402,
+          name: "Music",
+        },
+        {
+          id: 9648,
+          name: "Mystery",
+        },
+        {
+          id: 10749,
+          name: "Romance",
+        },
+        {
+          id: 878,
+          name: "Science Fiction",
+        },
+        {
+          id: 10770,
+          name: "TV Movie",
+        },
+        {
+          id: 53,
+          name: "Thriller",
+        },
+        {
+          id: 10752,
+          name: "War",
+        },
+        {
+          id: 37,
+          name: "Western",
+        },
+      ],
+    };
+  },
+
+  created() {
+    this.upcomingMovieList = axios
+      .get(
+        "https://api.themoviedb.org/3/movie/upcoming?api_key=8ec942643846f64d66eed102868455f3&language=en-US&region=US&page=1"
+      )
+      .then((info) => {
+        this.upcomingMovieList = info.data.results;
+      })
+      .then(() => {
+        this.upcomingMovieList = JSON.parse(
+          JSON.stringify(this.upcomingMovieList)
+        );
+        this.forceRerender();
+      });
+  },
+
+  methods: {
+    forceRerender() {
+      this.carouselKey += 1;
     },
-    // setup() {
-    //     const popupActive = ref(false);
-    //     const togglePopup = () => {
-    //         // sessionStorage.setItem("movieArray", this.recommendationMovieList);
-    //         popupActive.value = !popupActive.value;
-    //     }
-    //     return { popupActive, togglePopup };
-    // },
+    togglePopup(
+      posterPath,
+      originalTitle,
+      genreids,
+      releaseDate,
+      voteAverage,
+      overview,
+      movie
+    ) {
+      this.popupPoster = posterPath;
+      this.popupTitle = originalTitle;
+
+      this.popupReleaseDate = releaseDate;
+      this.popupAvarage = voteAverage;
+      this.popupOverview = overview;
+      this.popupActive = true;
+      //console.log(movie);
+      //movie.genre_ids
+      let genreName = "";
+      for (let i = 0; i < this.genreArray.length; i++) {
+        for (let j = 0; j < movie.genre_ids.length; j++) {
+          if (Number(this.genreArray[i].id) == Number(movie.genre_ids[j])) {
+            genreName += this.genreArray[i].name + ", ";
+          }
+        }
+      }
+      this.popupGenreIDs = genreName.slice(0, genreName.length - 2);
+    },
+    closePopup() {
+      this.popupActive = false;
+    },
+  },
+  // setup() {
+  //     const popupActive = ref(false);
+  //     const togglePopup = () => {
+  //         // sessionStorage.setItem("movieArray", this.recommendationMovieList);
+  //         popupActive.value = !popupActive.value;
+  //     }
+  //     return { popupActive, togglePopup };
+  // },
 };
 </script>
