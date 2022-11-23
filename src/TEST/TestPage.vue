00c8f99b2bc9fa62e395b54fe13d94e7d3027978
Marina Faria <marina.acfaria@gmail.com>
2022-11-22 16:27:36 -0800

    Update Test Page

--- src/TEST/TestPage.vue
+++ src/TEST/TestPage.vue
@@ -1,32 +1,32 @@
 <template>
-  <h1>Login Auth</h1>
-  <LoginAuth />
+    <h1>Login Auth</h1>
+    <LoginAuth />
 
-  <button @click.prevent="togglePopup">QUIT CHALLENGE</button>
-  <!-- QUIT CHALLENGE POPUP -->
-  <SimplePopup @close="togglePopup" :popupActive="popupActive">
-    <div class="popupContent">
-      <h1 class="popUpHeading">Quit?</h1>
-      <h3 class="popUpText">
-        If you quite the challenge,<br />you will lose all data.<br />Do you
-        wish to continue?
-      </h3>
-      <button class="primaryBtn" @click.prevent="deleteFromFirebase">
-        Quit Challenge
-      </button>
-    </div>
-  </SimplePopup>
-  <br />
-  <button @click.prevent="togglePopup">SAVE REVIEW</button>
-  <!-- QUIT CHALLENGE POPUP -->
-  <SimplePopup @close="togglePopup" :popupActive="popupActive">
-    <div class="popupContent">
-      <h1 class="popUpHeading">Your review has been saved!</h1>
-      <button @click.prevent="togglePopup" class="primaryBtn">Done</button>
-    </div>
-  </SimplePopup>
-  <h1>Login Auth</h1>
-  <LoginAuth />
+    <button @click.prevent="togglePopup">QUIT CHALLENGE</button>
+    <!-- QUIT CHALLENGE POPUP -->
+    <SimplePopup @close="togglePopup" :popupActive="popupActive">
+        <div class="popupContent">
+            <h1 class="popUpHeading">Quit?</h1>
+            <h3 class="popUpText">
+                If you quite the challenge,<br />you will lose all data.<br />Do you
+                wish to continue?
+            </h3>
+            <button class="primaryBtn" @click.prevent="deleteFromFirebase">
+                Quit Challenge
+            </button>
+        </div>
+    </SimplePopup>
+    <br />
+    <button @click.prevent="togglePopup">SAVE REVIEW</button>
+    <!-- QUIT CHALLENGE POPUP -->
+    <SimplePopup @close="togglePopup" :popupActive="popupActive">
+        <div class="popupContent">
+            <h1 class="popUpHeading">Your review has been saved!</h1>
+            <button @click.prevent="togglePopup" class="primaryBtn">Done</button>
+        </div>
+    </SimplePopup>
+    <h1>Login Auth</h1>
+    <LoginAuth />
 </template>
 
 <script>
@@ -35,25 +35,25 @@ import SimplePopup from "../components/SimplePopup.vue";
 import { ref } from "vue";
 
 export default {
-  name: "TestPage",
-  components: {
-    LoginAuth,
-    SimplePopup,
-  },
-  methods: {
-    redirect() {
-      this.$router.push("/");
+    name: "TestPage",
+    components: {
+        LoginAuth,
+        SimplePopup,
     },
-    deleteFromFirebase() {
-      console.log("deleted");
-    },
-  },
-  setup() {
-    const popupActive = ref(false);
-    const togglePopup = () => {
-      popupActive.value = !popupActive.value;
-    };
-    return { popupActive, togglePopup };
-  },
+    methods: {
+        redirect() {
+            this.$router.push("/");
+        },
+        deleteFromFirebase() {
+            console.log("deleted");
+        },
+        setup() {
+            const popupActive = ref(false);
+            const togglePopup = () => {
+                popupActive.value = !popupActive.value;
+            }
+            return { popupActive, togglePopup };
+        },
+    }
 };
 </script>
