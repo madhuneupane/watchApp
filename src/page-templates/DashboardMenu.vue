49b7f8fad73c5bced3ce504c4bdb1482be133edf
edenLangara <114768729+edenLangara@users.noreply.github.com>
2022-11-29 16:24:02 -0800

    Profile Page Styling

--- src/page-templates/DashboardMenu.vue
+++ src/page-templates/DashboardMenu.vue
@@ -1,220 +1,248 @@
 <template>
-  <section>
+  <section id="profilePage">
     <NavigationBar />
     <!-- <div v-if="FirstSection" class="fSect"> -->
-    <h1>Howdy, {{ nickname }}!</h1>
+    <div class="pageContentContainer">
+      <div class="profilePicAndMenuContainer">
+        <div class="profilePagePicContainer">
+          <h1>Howdy, {{ nickname }}!</h1>
 
-    <div v-if="secondPartFirst" class="profilePicSectionFirst">
-      <div class="profilePictureContainer">
-        <img id="profilePicture" :src="profilePic" alt="profilePic"
-          :class="{ capturedPicture: photoSnapped === true }" />
-      </div>
-      <img id="changing" src="../assets/icons/edit-photo.svg" alt="changeProfile" @click.prevent="clickImage" />
-    </div>
+          <div v-if="secondPartFirst" class="profilePicSectionFirst">
+            <div class="profilePictureContainer">
+              <img id="profilePicture" :src="profilePic" alt="profilePic"
+                :class="{ capturedPicture: photoSnapped === true }" />
+            </div>
+            <img id="changing" src="../assets/icons/edit-photo.svg" alt="changeProfile" @click.prevent="clickImage" />
+          </div>
 
-    <div v-if="secondPartSecond" class="profilePicSectionSecond">
-      <video autoplay class="feed"></video>
-      <button class="secondaryBtn" @click.prevent="displayImage">Snap</button>
-    </div>
+          <div v-if="secondPartSecond" class="profilePicSectionSecond">
+            <video autoplay class="feed"></video>
+            <button class="secondaryBtn" @click.prevent="displayImage">Snap</button>
+          </div>
 
-    <h2>{{ nickname }}</h2>
+          <h2>{{ nickname }}</h2>
+        </div>
+        
 
-    <div class="sideMenu">
-      <a @click="first">Favorite Genre</a>
+        <div class="sideMenu">
+          <a @click="first">Favorite Genre</a>
 
-      <a @click="second">Watched Movies</a>
+          <a @click="second">Watched Movies</a>
 
-      <a @click="third">Badges</a>
+          <a @click="third">Badges</a>
 
-      <a @click="fourth">Profile</a>
-    </div>
-
-    <div v-if="firstPart">
-      <h1>Edit Your preferences</h1>
-      <p>Select at least one genre</p>
-      <div class="genreSelectionContainer">
-        <div>
-          <input name="genre" type="checkbox" value="28" />
-          <label for="genre">Action</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="12" />
-          <label for="genre">Adventure</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="16" />
-          <label for="genre">Animation</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="35" />
-          <label for="genre">Comedy</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="80" />
-          <label for="genre">Crime</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="99" />
-          <label for="genre">Documentary</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="18" />
-          <label for="genre">Drama</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="10751" />
-          <label for="genre">Family</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="14" />
-          <label for="genre">Fantasy</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="36" />
-          <label for="genre">History</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="27" />
-          <label for="genre">Horror</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="10402" />
-          <label for="genre">Music</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="9648" />
-          <label for="genre">Mystery</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="10749" />
-          <label for="genre">Romance</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="878" />
-          <label for="genre">Science Fiction</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="10770" />
-          <label for="genre">TV Movie</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="53" />
-          <label for="genre">Thriller</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="1752" />
-          <label for="genre">War</label>
-        </div>
-        <div>
-          <input name="genre" type="checkbox" value="37" />
-          <label for="genre">Western</label>
+          <a @click="fourth">Profile</a>
         </div>
       </div>
-      <button type="cancel" class="secondaryBtn" @click="cancel">Cancel</button>
-      <button type="save" class="primaryBtn" @click="saveNewGenres">
-        Save
-      </button>
-    </div>
 
-    <div v-if="secondPart">
-      <section id="upcomingPageSec">
-        <h1>Movies you've watched</h1>
-        <p>Here are the list of movies you've seen from the challenges</p>
-        <SecondPopupModal :popupActive="popupActive" :popupTitle="popupTitle" :popupPoster="popupPoster"
-          :popupGenreIDs="popupGenreIDs" :popupReleaseDate="popupReleaseDate" :popupAvarage="popupAvarage"
-          :popupOverview="popupOverview" :popupRating="popupRating" :popupReview="popupReview"
-          v-on:closeClicked="closePopup">
-        </SecondPopupModal>
-        <div class="movieList">
-          <div v-for="(movies, i) in chalLoading" :key="i" class="ongoingChalItem">
-            <img :src="
-              'https://image.tmdb.org/t/p/w500' + movies.poster_path
-            " @click="
-  togglePopup(
-    movies.poster_path,
-    movies.original_title,
-    movies.genre_ids,
-    movies.release_date,
-    movies.vote_average,
-    movies.overview,
-    movies.rating,
-    movies.review,
-    movies
-  )
-" />
-            <!-- <h3>{{ movie[i].title }}</h3> -->
+
+      <div v-if="firstPart" id="profilePreferencesSec">
+        <h1 class="profilePageSecTitle">Edit Your preferences</h1>
+        <p>Select at least one genre</p>
+        <div class="genreSelectionContainer">
+          <div>
+            <input name="genre" type="checkbox" value="28" />
+            <label for="genre">Action</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="12" />
+            <label for="genre">Adventure</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="16" />
+            <label for="genre">Animation</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="35" />
+            <label for="genre">Comedy</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="80" />
+            <label for="genre">Crime</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="99" />
+            <label for="genre">Documentary</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="18" />
+            <label for="genre">Drama</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="10751" />
+            <label for="genre">Family</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="14" />
+            <label for="genre">Fantasy</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="36" />
+            <label for="genre">History</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="27" />
+            <label for="genre">Horror</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="10402" />
+            <label for="genre">Music</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="9648" />
+            <label for="genre">Mystery</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="10749" />
+            <label for="genre">Romance</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="878" />
+            <label for="genre">Science Fiction</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="10770" />
+            <label for="genre">TV Movie</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="53" />
+            <label for="genre">Thriller</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="1752" />
+            <label for="genre">War</label>
+          </div>
+          <div>
+            <input name="genre" type="checkbox" value="37" />
+            <label for="genre">Western</label>
           </div>
         </div>
-      </section>
-      <a class="seeMoreBtn link" @click.prevent="loadMore">Load More</a>
-    </div>
-
-    <div v-if="thirdPart">
-      <h1>Here are your badges!</h1>
-      <h2>Total of movies you've watched so far:</h2>
-      <h1>{{ this.points }}</h1>
-      <p>You have {{ movieLeft }} movies left to unlock the next badge</p>
-      <div v-if="badgeOne">
-        <img src="../assets/popcorn-badge.png" alt="" />
-        <h3>Popcorn</h3>
-        <p>10th Movie Milestone</p>
-      </div>
-      <div v-if="badgeTwo">
-        <img src="../assets/3d-glasses.png" alt="" />
-        <h3>3D Glasses</h3>
-        <p>20th Movie Milestone</p>
-      </div>
-      <div v-if="badgeThree">
-        <img src="../assets/mask-badge.png" alt="" />
-        <h3>Mask</h3>
-        <p>30th Movie Milestone</p>
+        <div class="profilePreferencesBtnContainer">
+          <button type="cancel" class="secondaryBtn cancelBtn" @click="cancel">Cancel</button>
+          <button type="save" class="primaryBtn saveBtn" @click="saveNewGenres">
+            Save
+          </button>
+        </div>
       </div>
 
-      <img src="../assets/no-badge.png" />
-      <h3>{{ nextBadge }}th movie badge</h3>
-      <p>Watch more movies to unlock this badge!</p>
-      <div class="btnContainer">
-        <button type="save" class="primaryBtn" @click="badgesPopup">
-          Badges
+      <div v-if="secondPart" id="profilePageWatchedMovies">
+        <section>
+          <h1 class="profilePageSecTitle">Movies you've watched</h1>
+          <p class="profilePageSecDescription">Here are the list of movies you've seen from the challenges</p>
+          <SecondPopupModal :popupActive="popupActive" :popupTitle="popupTitle" :popupPoster="popupPoster"
+            :popupGenreIDs="popupGenreIDs" :popupReleaseDate="popupReleaseDate" :popupAvarage="popupAvarage"
+            :popupOverview="popupOverview" :popupRating="popupRating" :popupReview="popupReview"
+            v-on:closeClicked="closePopup">
+          </SecondPopupModal>
+          <div class="profilePageWatchedMovieList">
+            <div v-for="(movies, i) in chalLoading" :key="i" class="movieWatched">
+              <img :src="
+                'https://image.tmdb.org/t/p/w500' + movies.poster_path
+              " @click="
+                togglePopup(
+                  movies.poster_path,
+                  movies.original_title,
+                  movies.genre_ids,
+                  movies.release_date,
+                  movies.vote_average,
+                  movies.overview,
+                  movies.rating,
+                  movies.review,
+                  movies
+                )
+              " />
+              <!-- <h3>{{ movie[i].title }}</h3> -->
+            </div>
+          </div>
+          <a class="seeMoreBtn link" @click.prevent="loadMore">Load More</a>
+        </section>
+      </div>
+
+      <div v-if="thirdPart" id="profileBadgesSec">
+        <div class="badgesSecDescriptionContainer">
+          <h1 class="badgesSecTitle">Here are your badges!</h1>
+          <h1 class="badgesSecDescription">Total of movies you've watched so far:</h1>
+          <h1 class="badgesSecPoints">{{ this.points }}</h1>
+          <p class="badgesSecPointsLeft">You have {{ movieLeft }} movies left to unlock the next badge</p>
+        </div>
+        <div class="badgesContainer">
+          <div class="badgeInfo" v-if="badgeOne">
+            <img src="../assets/popcorn-badge.png" alt="" />
+            <div class="badgeDescription">
+              <h3 class="badgeName">Popcorn</h3>
+              <p>10th Movie Milestone</p>
+            </div>
+          </div>
+          <div class="badgeInfo" v-if="badgeTwo">
+            <img src="../assets/3d-glasses.png" alt="" />
+            <div class="badgeDescription">
+              <h3 class="badgeName">3D Glasses</h3>
+              <p>20th Movie Milestone</p>
+            </div>
+          </div>
+          <div class="badgeInfo" v-if="badgeThree">
+            <img src="../assets/mask-badge.png" alt="" />
+            <div class="badgeDescription">
+              <h3 class="badgeName">Mask</h3>
+              <p>30th Movie Milestone</p>
+            </div>
+          </div>
+
+          <div class="badgeInfo">
+            <img src="../assets/no-badge.png" />
+            <div class="badgeDescription">
+              <h3 class="badgeName">{{ nextBadge }}th movie badge</h3>
+              <p>Watch more movies to unlock this badge!</p>
+            </div>
+          </div>
+        </div>
+        
+        <div class="btnContainer">
+          <button type="save" class="primaryBtn" @click="badgesPopup">
+            Badges
+          </button>
+          <button type="save" class="primaryBtn" @click="sharedBadgesPopup">
+            Shared Badges
+          </button>
+          <BadgesPopup @close="firstTogglePopup" :popupActive="firstpopupActive">
+            <div class="popupContent">
+              <h1 class="popUpHeading">Popcorn</h1>
+              <img src="#" alt="#" />
+              <h3 class="popUpText">20th movie milestone</h3>
+              <button @click="redirect" type="button" class="secondaryBtn">
+                Share Badge
+              </button>
+            </div>
+          </BadgesPopup>
+          <SharedBadgesPopup @close="secondTogglePopup" :popupActive="secondpopupActive">
+            <div class="popupSecondContent">
+              <h1 class="popUpHeading">Unlock at 130th movie</h1>
+              <img src="#" alt="#" />
+              <h3 class="popUpText">Watched more movies to unlock this badge</h3>
+              <button @click="redirect" type="button" class="secondaryBtn">
+                Watch more movies
+              </button>
+            </div>
+          </SharedBadgesPopup>
+        </div>
+      </div>
+
+      <div v-if="fourthPart" id="accountProfile">
+        <h1 class="accountSecTitle">Your Account</h1>
+        <div class="accountInfoContainer">
+          <div class="accountNameContainer">
+            <span class="userNameLabel">Name:</span>
+            <span class="userName">{{ this.fname + " " + this.lname }}</span>
+          </div>
+          <div class="accountNicknameContainer">
+            <span class="userNicknameLabel">Nickname:</span>
+            <span class="userNickname">{{ nickname }}</span>
+          </div>
+        </div>
+        <button type="submit" class="primaryBtn accountInfoSaveBtn" @click.prevent="resetPassword">
+          Change Password
         </button>
-        <button type="save" class="primaryBtn" @click="sharedBadgesPopup">
-          Shared Badges
-        </button>
-        <BadgesPopup @close="firstTogglePopup" :popupActive="firstpopupActive">
-          <div class="popupContent">
-            <h1 class="popUpHeading">Popcorn</h1>
-            <img src="#" alt="#" />
-            <h3 class="popUpText">20th movie milestone</h3>
-            <button @click="redirect" type="button" class="secondaryBtn">
-              Share Badge
-            </button>
-          </div>
-        </BadgesPopup>
-        <SharedBadgesPopup @close="secondTogglePopup" :popupActive="secondpopupActive">
-          <div class="popupSecondContent">
-            <h1 class="popUpHeading">Unlock at 130th movie</h1>
-            <img src="#" alt="#" />
-            <h3 class="popUpText">Watched more movies to unlock this badge</h3>
-            <button @click="redirect" type="button" class="secondaryBtn">
-              Watch more movies
-            </button>
-          </div>
-        </SharedBadgesPopup>
       </div>
-    </div>
-
-    <div v-if="fourthPart" class="accountProfile">
-      <h1>Your Account</h1>
-      <section>
-        <div>Name:</div>
-        <div>{{ this.fname + " " + this.lname }}</div>
-        <br />
-        <div>Nickname:</div>
-        <div>{{ nickname }}</div>
-      </section>
-      <button type="submit" class="primaryBtn" @click.prevent="resetPassword">
-        Change Password
-      </button>
     </div>
 
     <FooterBar />
